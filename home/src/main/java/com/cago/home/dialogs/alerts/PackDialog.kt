b6566e583024f07c29139363b2525ba329c9708c
Vlad P <petrof.v.a.0910@gmail.com>
2022-05-22 11:24:48 +0200

    fix pack dialog

--- home/src/main/java/com/cago/home/dialogs/alerts/PackDialog.kt
+++ home/src/main/java/com/cago/home/dialogs/alerts/PackDialog.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cago.core.R
 import com.cago.core.databinding.DialogPackBinding
 import com.cago.core.dialogs.BaseDialog
+import com.cago.core.utils.InputType
 
 class PackDialog(
     private val listener: (String)->Unit 
@@ -18,10 +20,17 @@ class PackDialog(
     ): View {
         val binding = DialogPackBinding.inflate(inflater)
         binding.apply{
-            packName.requestFocus()
+            name.requestFocus()
             positive.setOnClickListener {
-                listener(packName.text.toString())
-                closeDialog()
+                if (!name.text.isNullOrBlank()) {
+                    listener(name.text.toString())
+                    closeDialog()
+                } else {
+                    name.apply{
+                        error = context.getString(R.string.invalid_name)
+                        requestFocus()
+                    }
+                }
             }
             negative.setOnClickListener { closeDialog() }
         }
